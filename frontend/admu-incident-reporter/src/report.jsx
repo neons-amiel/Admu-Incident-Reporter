abbfdfcda0cce1efb09125840444f267f9289641
neons-amiel <robertamiel.abadilla@gmail.com>
2025-12-03 03:30:33 +0800

    Ready for Final Submission

--- frontend/admu-incident-reporter/src/report.jsx
+++ frontend/admu-incident-reporter/src/report.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'; // 1. Import useEffect
+import React, { useState, useEffect } from 'react'; 
 import { Link, useNavigate } from 'react-router-dom';
 import admulogo from './logos/admu_logo.png';
 
